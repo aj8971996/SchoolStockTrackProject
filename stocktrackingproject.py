e764d9a88e65245977eb26e0efb6e1c18b09320f
Alex Barkus <alexander.barkus96@gmail.com>
2022-09-11 11:41:23 -0700

    Module 2

---
+++ stocktrackingproject.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 15:17:27 2022
+
+@author: 17025
+"""
+
+"""
+
+Class Project - Stock Tracking Software 
+Starting Time 1: on 09/01/2022 @~ 03:00 PM PT 
+
+"""
+
